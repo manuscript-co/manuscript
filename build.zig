50aaaa922fd3684dcc56facd0eaf9a62964981ad
Chinmay Kulkarni <chinmaymk@users.noreply.github.com>
2023-07-13 08:33:47 -0700

    fix: jsc debug info

--- build.zig
+++ build.zig
@@ -2,7 +2,15 @@ const std = @import("std");
 const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) !void {
-    const e = b.addExecutable(.{ .name = "msr", .root_source_file = .{ .path = "src/msr.zig" } });
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const e = b.addExecutable(.{
+        .name = "msr",
+        .root_source_file = .{ .path = "src/msr.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     e.addIncludePath("staging/cpython/include/python3.13d");
     e.addAssemblyFile("staging/cpython/lib/libpython3.13d.a");
     b.installArtifact(e);
--- tools/mac.build.jsc.sh
+++ tools/mac.build.jsc.sh
@@ -1,11 +1,11 @@
-rm -rf staging/jsc
+# rm -rf staging/jsc
 mkdir -p staging/jsc
 cd staging/jsc
 
 CXXFLAGS="-pthread" cmake -DPORT="JSCOnly" -G "Ninja" \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
   -DENABLE_STATIC_JSC=ON \
-  -DCMAKE_BUILD_TYPE=RelWithDebugInfo \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
   -DDEVELOPER_MODE=ON \
   -DENABLE_FTL_JIT=ON ../../deps/Webkit
 
