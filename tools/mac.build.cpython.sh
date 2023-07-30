ea5370f961c9b35ffc123a9a9d90a5a25be4e4f1
Chinmay Kulkarni <chinmaymk@users.noreply.github.com>
2023-07-30 11:36:58 -0700

    fix: #3 statically link modules

--- tools/mac.build.cpython.sh
+++ tools/mac.build.cpython.sh
@@ -3,5 +3,10 @@ cd deps/cpython
 
 GDBM_CFLAGS="-I$(brew --prefix gdbm)/include" \
 GDBM_LIBS="-L$(brew --prefix gdbm)/lib -lgdbm" \
-./configure --with-pydebug --with-openssl="$(brew --prefix openssl@3.0)" --prefix=$(realpath ../../staging/cpython) MODULE_BUILDTYPE=static -q
+./configure --with-pydebug \
+  --with-openssl="$(brew --prefix openssl@3.0)" \
+  --prefix=$(realpath ../../staging/cpython) \
+  --disable-test-modules \
+  MODULE_BUILDTYPE=static -q 
+
 make -s -j8 install
