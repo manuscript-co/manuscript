ca48d7968be78a72fe61708ae1fc60f1801161ae
Chinmay Kulkarni <chinmaymk@users.noreply.github.com>
2023-08-01 00:07:16 -0700

    (#7) #14 staging prep

--- build.zig
+++ build.zig
@@ -6,21 +6,22 @@ pub fn build(b: *Builder) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const staging = b.getInstallPath(.prefix, "staging");
     const mrt = b.addExecutable(.{ 
         .name = "mrt", 
         .root_source_file = std.build.FileSource.relative("src/mrt.zig"), 
         .target = target, 
         .optimize = optimize 
     });
-    mrt.addIncludePath(try rp(b, &.{ "staging", "cpython", "include", "python3.12d" }));
 
+    mrt.addIncludePath(try rp(b, &.{staging, "cpython", "include", "python3.12d" }));
     mrt.addIncludePath(try rp(b, &.{ "src", "101" }));
 
     // python
-    mrt.addAssemblyFile(try rp(b, &.{ "staging", "cpython", "lib", "libpython3.12d.a" }));
+    mrt.addAssemblyFile(try rp(b, &.{ staging, "cpython", "lib", "libpython3.12d.a" }));
     // javascript
-    mrt.addAssemblyFile(try rp(b, &.{ "staging", "101", "lib101.a" }));
-    mrt.addAssemblyFile(try rp(b, &.{ "staging", "v8", "obj", "libv8_monolith.a" }));
+    mrt.addAssemblyFile(try rp(b, &.{ staging, "101", "lib101.a" }));
+    mrt.addAssemblyFile(try rp(b, &.{ staging, "v8", "obj", "libv8_monolith.a" }));
     mrt.linkLibCpp();
 
     const prep = b.option(bool, "prep-staging", "builds v8, cpython, 101");
