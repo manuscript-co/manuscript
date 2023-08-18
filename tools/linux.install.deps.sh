686a9d30864ea2c8131130ee9168f2226ba08c53
Chinmay Kulkarni <chinmaymk@users.noreply.github.com>
2023-08-18 11:02:29 -0700

    #35 linux workflows

---
+++ tools/linux.install.deps.sh
@@ -0,0 +1,12 @@
+
+apt-get update
+
+apt install xz-utils wget \
+    generate-ninja=0.0~git20220118.0725d78-1 \
+    ninja-build=1.10.1-1 \
+    build-essential=12.9ubuntu3 \
+    ccache=4.5.1-1 \
+    make=4.3-4.1build1 \
+    zlib1g-dev=1:1.2.11.dfsg-2ubuntu9.2 \
+    pkg-config=0.29.2-1ubuntu3 \
+    -y
